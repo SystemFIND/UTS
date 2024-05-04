e9d84fd65a60ef1879bf229e9436da76f6290da1
SystemFIND <scopsilver@gmail.com>
2024-05-04 21:00:40 +0700

    number 2 is done !!!

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,6 +1,12 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
 
+// Untuk menghitung jumlah login yang gagal
+const loginAttempts = new Map();
+
+const maxAttempts = 5; // Maximum Wrong login
+const duration = 30 * 60 * 1000; //Durasi tidak bisa login
+
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -12,6 +18,22 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
+    // mengecek apakah user dikenakan block atau tidak
+    const Blocked = loginAttempts.get(email);
+    if (Blocked && Blocked.attempts >= maxAttempts) {
+      const time = Date.now(); // mengambil waktu real time
+
+      if (time - Blocked.lastAttempts < duration) {
+        throw errorResponder(
+          errorTypes.FORBIDDEN,
+          'Too many login attemps. Please try again later.'
+        );
+      } else {
+        // Reset jumlah login attemps
+        loginAttempts.delete(email);
+      }
+    }
+
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -19,6 +41,11 @@ async function login(request, response, next) {
     );
 
     if (!loginSuccess) {
+      // increments for wrong login, start from 0
+      const attempts = (loginAttempts.get(email)?.attempts || 0) + 1;
+      const lastAttempts = Date.now();
+      loginAttempts.set(email, { attempts, lastAttempts });
+
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
         'Wrong email or password'
